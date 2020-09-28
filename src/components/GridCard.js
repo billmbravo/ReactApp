092c1cc7457fc12b24c4a2e0f3beacb8646fdbaf
Bill <bmbravo@outlook.com>
2020-09-27 22:42:55 -0500

    mild changes to Formulario and GridCard

--- src/components/GridCard.js
+++ src/components/GridCard.js
@@ -8,64 +8,80 @@ import ListadoPrueba from './ListadoPrueba';
 const url = 'http://testapi.beatlech.com/public/api/personas/';
 
 class GridCard extends React.Component {
-    state = {
-        id:null,
-        nombre: null,
-        apellido: null,
-        anio_nacimiento: null,
-    };
-    handleChange = (e) =>{
-        this.setState({
-            [e.target.id]: e.target.value,
-        });
-    };
+	state = {
+		id: null,
+		nombre: null,
+		apellido: null,
+		anio_nacimiento: null
+	};
+	handleChange = (e) => {
+		this.setState({
+			[e.target.id]: e.target.value
+		});
+	};
 
-    handleClick = async (id)=>{
-        const requestOptions = {
-            method: 'POST',
-            headers : {'Content-Type': 'appliation/json'},
-            body: JSON.stringify({
-                nombre: this.state.nombre,
-                apellido: this.state.apellido,
-                anio_nacimiento: this.state.anio_nacimiento,
-            }),
-        };
-        
-        fetch(url, requestOptions)
-        .then((res)=> res.json())
-        .catch((error)=>console.error('Error:', error))
-        .then((response)=>console.log('success:', response));
-    };
-    handleUpdate = async (e)=>{
-        const requestOptions = {
-            method: 'PUT',
-            headers : {'Content-Type': 'appliation/json'},
-            body: JSON.stringify({
-                nombre: this.state.nombre,
-                apellido: this.state.apellido,
-                anio_nacimiento: this.state.anio_nacimiento,
-            }),
-        };
-        
-        fetch(url+this.state.id, requestOptions)
-        .then((res)=> res.json())
-        .catch((error)=>console.error('Error:', error))
-        .then((response)=>console.log('success:', response));
-    };
-    render() {
-        return (
-            <div className='container'>
-                <div className='row'>
-                    <div className='col-sm-4'>
-                        <ListadoPrueba/>
-                    </div>
-                    <div className='col-sm-6'>
-                        <Formulario OnChange={this.handleChange} onClick={this.handleClick} onClickUpdate={this.handleUpdate}/>
-                    </div>
-                </div>
-            </div> 
-        );
-    }
+	handleClick = async (id) => {
+		const requestOptions = {
+			method: 'POST',
+			headers: { 'Content-Type': 'appliation/json' },
+			body: JSON.stringify({
+				nombre: this.state.nombre,
+				apellido: this.state.apellido,
+				anio_nacimiento: this.state.anio_nacimiento
+			})
+		};
+
+		fetch(url, requestOptions)
+			.then((res) => res.json())
+			.catch((error) => console.error('Error:', error))
+			.then((response) => console.log('success:', response));
+	};
+	handleUpdate = async (e) => {
+		const requestOptions = {
+			method: 'PUT',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({
+				nombre: this.state.nombre,
+				apellido: this.state.apellido,
+				anio_nacimiento: this.state.anio_nacimiento
+			})
+		};
+
+		fetch(url + this.state.id, requestOptions)
+			.then((res) => res.json())
+			.catch((error) => console.error('Error:', error))
+			.then((response) => console.log('success:', response));
+	};
+	handleDelete = async (e) => {
+		const requestOptions = {
+			method: 'DELETE',
+			headers: { 'Content-Type': 'application/json' }
+		};
+
+		fetch(url + this.state.id, requestOptions)
+			.then((res) => res.json())
+			.catch((error) => console.error('Error:', error))
+			.then((response) => console.log('success:', response));
+	};
+	render() {
+		return (
+			<div className="container">
+				<div className="row">
+					<div className="col-sm-4">
+						<ListadoPrueba />
+					</div>
+					<div className="col-sm-6">
+						<Formulario
+							OnChange={this.handleChange}
+							onClick={this.handleClick}
+							onClickUpdate={this.handleUpdate}
+							onClickDelete={this.handleDelete}
+						/>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default GridCard;
